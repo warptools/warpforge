7aa0f92f7efa4e35e959e0b78529b7ca1d6d7f54
Eric Evenchick <eric@evenchick.com>
2021-08-09 14:06:10 -0700

    create warpforge dir

--- install-uroot.sh
+++ install-uroot.sh
@@ -1,2 +1,3 @@
 UROOT=plugins/uroot.tar.gz
+mkdir -p $HOME/.warpforge/warehouse
 rio mirror --source=file://$UROOT --target=ca+file://$HOME/.warpforge/warehouse `rio scan tar --source=file://$UROOT`
