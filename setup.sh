8e05c8f1808b96c0f58be6dc051953e9502e125d
Eric Evenchick <eric@evenchick.com>
2021-08-18 14:58:45 -0700

    fix bootstrap

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 mkdir -p $HOME/.warpforge/warehouse
-rio mirror --source=file://plugins/alpine.tgz --target=ca+file://$HOME/.warpforge/warehouse tar:7KnxDuMATmnoQ1VFCLzxTT66Pdh7DfqJ5UGQSBniFFhD2yyAtfpuKNcdFf8Titvxxr
+./plugins/rio mirror --source=file://plugins/alpine.tgz --target=ca+file://$HOME/.warpforge/warehouse tar:47Yg1Sdq21rPyDw9X9sCmRubQUADhFKe9G7qZCJRe61RhWPCxcQysCFzyCHffBKRjB
 
 mkdir -p $HOME/.warpforge/bin
-cp plugins/runc $HOME/.warpforge/bin
+cp plugins/rio $HOME/.warpforge/bin/
+cp plugins/runc $HOME/.warpforge/bin/
