b7c37f12a41676858ea33467d6771e9a5e28e236
Eric Myhre <hash@exultant.us>
2021-08-20 05:01:10 +0200

    Numerous fixes upstream.  Formula parse and load now testably much more correct.

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/frankban/quicktest v1.11.3
-	github.com/ipld/go-ipld-prime v0.12.1-0.20210819225834-a47ecf924a39
+	github.com/ipld/go-ipld-prime v0.12.1-0.20210820025617-575b9588eeb8
 	github.com/opencontainers/runtime-spec v1.0.2 // indirect
 	github.com/urfave/cli/v2 v2.3.0
 	mvdan.cc/sh/v3 v3.3.0
--- wfapi/formula_test.go
+++ wfapi/formula_test.go
@@ -42,4 +42,30 @@ func TestParseFormulaAndContext(t *testing.T) {
 	frmAndCtx := FormulaAndContext{}
 	_, err := ipld.Unmarshal([]byte(serial), json.Decode, &frmAndCtx, TypeSystem.TypeByName("FormulaAndContext"))
 	qt.Assert(t, err, qt.IsNil)
+
+	ports := []SandboxPort{
+		SandboxPort{SandboxPath: func() *SandboxPath { v := SandboxPath("mount/path"); return &v }()},
+		SandboxPort{VariableName: func() *VariableName { v := VariableName("ENV_VAR"); return &v }()},
+		SandboxPort{SandboxPath: func() *SandboxPath { v := SandboxPath("more/mounts"); return &v }()},
+	}
+	inputs := frmAndCtx.Formula.Inputs
+	qt.Assert(t, inputs.Keys, qt.DeepEquals, ports)
+	// Okay, this is going to look a little funny, and it's because we've got another upstream bug to fix.
+	// `inputs.Values[ports[0]]` isn't enough ... because SandboxPort has pointers in it... so, key equality fail.
+	// But if we use the values from the keys slice, those are pointer equality all the way down, so it works.
+	qt.Check(t, inputs.Values[inputs.Keys[0]], qt.DeepEquals,
+		FormulaInput{FormulaInputSimple: &FormulaInputSimple{WareID: &WareID{"tar", "qwerasdf"}}},
+	)
+	qt.Check(t, inputs.Values[inputs.Keys[1]], qt.DeepEquals,
+		FormulaInput{FormulaInputSimple: &FormulaInputSimple{Literal: func() *string { v := "hello"; return &v }()}},
+	)
+	qt.Check(t, inputs.Values[inputs.Keys[2]], qt.DeepEquals,
+		FormulaInput{FormulaInputComplex: &FormulaInputComplex{
+			Basis: FormulaInputSimple{WareID: &WareID{"tar", "fghjkl"}},
+			Filters: FilterMap{
+				Keys:   []string{"uid"},
+				Values: map[string]string{"uid": "10"},
+			},
+		}},
+	)
 }
