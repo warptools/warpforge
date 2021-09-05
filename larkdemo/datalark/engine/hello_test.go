e8244b4427be7631937e2f013461bff38379a92c
Eric Myhre <hash@exultant.us>
2021-09-05 12:13:38 +0200

    larkery: change the way linebreaks work in test output.
    Doing it by putting a trailing "\n" in the print call causes
    a space to be printed before the linebreak...
    which will cause fairly infuriating results when trying to understand
    why your golang example test says things aren't equal.
    (Golang doesn't quote this at all, so it's... invisible.)
    (Also, it only shows up when there's more than one line!  Otherwise
    the whitespace stripper noms it.  Whee.)
    
    That... was really painful to discover.

--- larkdemo/datalark/engine/hello_test.go
+++ larkdemo/datalark/engine/hello_test.go
@@ -12,18 +12,18 @@ func Example_hello() {
 		Print: func(thread *starlark.Thread, msg string) {
 			//caller := thread.CallFrame(1)
 			//fmt.Printf("%s: %s: %s\n", caller.Pos, caller.Name, msg)
-			fmt.Printf(msg)
+			fmt.Printf("%s\n", msg)
 		},
 	}
 	_, err := starlark.ExecFile(thread, "thefilename.star", `
-print(ConstructString("yo"), "\n")
+print(ConstructString("yo"))
 x = {"bz": "zoo"}
 
 def uwot():
 	return {"ay": "zee"}
 
-print(ConstructMap(hey="hai", zonk="wot", **uwot()), "\n")
-#print(ConstructMap({ConstructString("fun"): "heeey"}), "\n")
+print(ConstructMap(hey="hai", zonk="wot", **uwot()))
+#print(ConstructMap({ConstructString("fun"): "heeey"}))
 
 export = {"uwot": uwot} # i kinda like npm's style for this better than starlark's "every global becomes exported".
 # we'd have to see if it's actually viable to wrap the built-in load, too.  i don't see why not, though.
