f90562a72be3f65145092d53db1d90a78e4bc611
Eric Myhre <hash@exultant.us>
2021-09-05 12:22:30 +0200

    larkery: drop comments about modules and exports.
    Can revisit that thought later.

--- larkdemo/datalark/engine/hello_test.go
+++ larkdemo/datalark/engine/hello_test.go
@@ -24,17 +24,6 @@ def uwot():
 
 print(ConstructMap(hey="hai", zonk="wot", **uwot()))
 #print(ConstructMap({ConstructString("fun"): "heeey"}))
-
-export = {"uwot": uwot} # i kinda like npm's style for this better than starlark's "every global becomes exported".
-# we'd have to see if it's actually viable to wrap the built-in load, too.  i don't see why not, though.
-# the naming can become desynchronized, and syntactically mildly redundant, but... i really don't see a problem there.
-# see https://github.com/google/starlark-go/blob/master/doc/spec.md#name-binding-and-variables .
-#
-# apparently you can also use nested function def's to hide things from children, but... uuf?
-# i wanna see demos of how to make that not painful and surprising.  and then maybe make our datalark builtins act the same way to avoid weirdness.
-#
-# ... you can probably just... not worry about any of this, for many months.
-
 `,
 		// FUTURE: may want to make a module, interpret it first, and then make it available in the globals...
 		//  as a sheer way of getting dotted notation in a consistent way.
