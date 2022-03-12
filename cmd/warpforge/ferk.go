fc0d43f1783ffa746aaf2999ad0c41002db2b2f3
Eric Evenchick <eric@evenchick.com>
2022-03-12 13:11:50 +0000

    add options for ferk
    change default shell to bash

--- cmd/warpforge/ferk.go
+++ cmd/warpforge/ferk.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/ipld/go-ipld-prime"
 	"github.com/ipld/go-ipld-prime/codec/json"
@@ -15,12 +17,28 @@ var ferkCmdDef = cli.Command{
 	Name:   "ferk",
 	Usage:  "Idk yet",
 	Action: cmdFerk,
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name: "rootfs",
+		},
+		&cli.StringFlag{
+			Name: "cmd",
+		},
+	},
 }
 
-var ferkPlot = `
+type ferkPlot struct {
+	Rootfs  string
+	Shell   string
+	Persist bool
+}
+
+const defaultRootfs = "catalog:warpsys.org/bootstrap-rootfs:bullseye-1646092800:amd64"
+
+const ferkPlotTemplate = `
 {
         "inputs": {
-                "rootfs": "catalog:warpsys.org/bootstrap-rootfs:bullseye-1646092800:amd64"
+                "rootfs": "literal:none"
         },
         "steps": {
                 "ferk": {
@@ -52,16 +70,37 @@ func cmdFerk(c *cli.Context) error {
 		return err
 	}
 
+	// generate the basic default plot from json template
 	plot := wfapi.Plot{}
-	_, err = ipld.Unmarshal([]byte(ferkPlot), json.Decode, &plot, wfapi.TypeSystem.TypeByName("Plot"))
+	_, err = ipld.Unmarshal([]byte(ferkPlotTemplate), json.Decode, &plot, wfapi.TypeSystem.TypeByName("Plot"))
 	if err != nil {
-		return err
+		return fmt.Errorf("error parsing template plot: %s", err)
 	}
 
+	// convert rootfs input string to PlotInput
+	// this requires additional quoting to be parsed correctly by ipld
+	rootfsStr := fmt.Sprintf("\"%s\"", defaultRootfs)
+	if c.String("rootfs") != "" {
+		// custom value provided, override default
+		rootfsStr = fmt.Sprintf("\"%s\"", c.String("rootfs"))
+	}
+	rootfs := wfapi.PlotInput{}
+	_, err = ipld.Unmarshal([]byte(rootfsStr), json.Decode, &rootfs, wfapi.TypeSystem.TypeByName("PlotInput"))
+	if err != nil {
+		return fmt.Errorf("error parsing rootfs input: %s", err)
+	}
+	plot.Inputs.Values["rootfs"] = rootfs
+
+	// set command to execute
+	if c.String("cmd") != "" {
+		plot.Steps.Values["ferk"].Protoformula.Action.Exec.Command = strings.Split(c.String("cmd"), " ")
+	}
+
+	// create the persist directory, if it does not exist
 	os.MkdirAll("wf-persist", 0755)
 
+	// run the plot in interactive mode
 	config := wfapi.PlotExecConfig{
-		Recursive:   c.Bool("recursive"),
 		Interactive: true,
 	}
 	_, err = plotexec.Exec(wss, plot, config, logger)
