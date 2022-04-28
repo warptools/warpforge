1957f368a25c6b39c0a4134d4db3f2b9ff56ab9f
Eric Evenchick <eric@evenchick.com>
2022-04-28 14:37:59 +0000

    fix quickstart rootfs ref

--- cmd/warpforge/quickstart.go
+++ cmd/warpforge/quickstart.go
@@ -18,7 +18,7 @@ var quickstartCmdDef = cli.Command{
 
 const defaultPlotJson = `{
 	"inputs": {
-		"rootfs": "catalog:alpinelinux.org/alpine:v3.15.0:x86_64"
+		"rootfs": "catalog:warpsys.org/alpinelinux/rootfs:v3.15.4:amd64"
 	},
 	"steps": {
 		"hello-world": {
