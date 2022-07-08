7de63e8bc3149659d9a2d528008a1cc654bed98a
Eric Myhre <hash@exultant.us>
2022-07-08 13:13:24 -0700

    WIP2

--- larkdemo/hello_test.go
+++ larkdemo/hello_test.go
@@ -20,7 +20,9 @@ func ExampleHello() {
 		//  We do need to know the concrete types.  But we should usually only need to say that once in the whole program.
 		//   ... ohhey, Rod introduced a bindnode registry recently.  That might be just the thing.
 		bindnode.Prototype((*wfapi.Plot)(nil), wfapi.TypeSystem.TypeByName("Plot")),
+		bindnode.Prototype((*wfapi.Step)(nil), wfapi.TypeSystem.TypeByName("Step")),
 		bindnode.Prototype((*wfapi.Protoformula)(nil), wfapi.TypeSystem.TypeByName("Protoformula")),
+		bindnode.Prototype((*wfapi.Action)(nil), wfapi.TypeSystem.TypeByName("Action")),
 		bindnode.Prototype((*wfapi.Action_Script)(nil), wfapi.TypeSystem.TypeByName("Action_Script")),
 	}))
 
@@ -33,18 +35,18 @@ result = Plot(
 		"two": "literal:foobar",
 	},
 	steps={
-		"beep": Protoformula(
+		"beep": Step(Protoformula( # FIXME should also work without 'Step(' wrapper, shouldn't it?
 			inputs={
 				"/": "pipe::one",
 				"$FOO": "pipe::two",
 			},
-			action=Action_Script( # FIXME handing in a type that's a union member here should work, like this.  currently doesn't?  appears to be trying to copy the content
+			action=Action(Action_Script( # FIXME handing in a type that's a union member here should work, without the 'Action(' wrapper.  currently doesn't?  appears to be trying to copy the content if I drop the 'Action(' constructor, which ends in the NodeAssembler for an Action getting the key "interpreter" thrown at it, which of course (correctly) doesn't fly.
 				interpreter="/bin/bash", # todo soon, make a wrapper func where this kwarg has a default
 				contents=["hey", "hello"],
 				network=False,
-			),
+			)),
 			outputs={},
-		)
+		))
 	},
 	outputs={},
 )
@@ -65,7 +67,29 @@ result = Plot(
 	// 		}}}
 	// 		string<LocalLabel>{"two"}: union<PlotInput>{union<PlotInputSimple>{string<Literal>{"foobar"}}}
 	// 	}
-	// 	steps: map<Map__StepName__Step>{}
+	// 	steps: map<Map__StepName__Step>{
+	// 		string<StepName>{"beep"}: union<Step>{struct<Protoformula>{
+	// 			inputs: map<Map__SandboxPort__PlotInput>{
+	// 				union<SandboxPort>{string<SandboxPath>{""}}: union<PlotInput>{union<PlotInputSimple>{struct<Pipe>{
+	// 					stepName: string<StepName>{""}
+	// 					label: string<LocalLabel>{"one"}
+	// 				}}}
+	// 				union<SandboxPort>{string<SandboxVar>{"FOO"}}: union<PlotInput>{union<PlotInputSimple>{struct<Pipe>{
+	// 					stepName: string<StepName>{""}
+	// 					label: string<LocalLabel>{"two"}
+	// 				}}}
+	// 			}
+	// 			action: union<Action>{struct<Action_Script>{
+	// 				interpreter: string<String>{"/bin/bash"}
+	// 				contents: list<List__String>{
+	// 					0: string<String>{"hey"}
+	// 					1: string<String>{"hello"}
+	// 				}
+	// 				network: bool<Bool>{false}
+	// 			}}
+	// 			outputs: map<Map__LocalLabel__GatherDirective>{}
+	// 		}}
+	// 	}
 	// 	outputs: map<Map__LocalLabel__PlotOutput>{}
 	// }
 }
