450b5e6544b2115a2c8b6752a066780f8fd552b8
Eric Myhre <hash@exultant.us>
2022-08-26 20:29:04 +0100

    'workspace inspect' command now generates many checkmarks.
    Okay, not checkmarks yet.  I'm going to prettify this later.
    
    But it does a bunch of interesting accounting.
    
    And also, yes, having the ability to say "--gohard=false" is already
    relevant in its performance impacts.  In the 29 modules currently
    in the warpsys workspace... it takes 0m0.008s real wallclock time
    to just enumerate them (makes sense; it's mostly walk, and just open
    and parse on the module file itself to get the name)...
    
    ... and it takes a whopping 0m0.390s real wallclock seconds to collect
    all these connectivity and health checks.  Like, it's *noticable*.
    
    Mind, I suspect there's very low-hanging fruit for improving that.
    I notice the system time is 0m0.273s of that (and 0m0.133s user time
    from the same sample), so... I suspect even just throwing some caching
    around the reads of the catalog would probably make a big difference.
    
    Tracing might tell us more (but that work is going on parallel to this,
    so, more on that later perhaps).

--- cmd/warpforge/winspect.go
+++ cmd/warpforge/winspect.go
@@ -6,9 +6,10 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/warpfork/warpforge/pkg/dab"
-
 	"github.com/urfave/cli/v2"
+
+	"github.com/warpfork/warpforge/pkg/dab"
+	"github.com/warpfork/warpforge/pkg/plotexec"
 )
 
 var cmdDefWorkspaceInspect = cli.Command{
@@ -16,6 +17,13 @@ var cmdDefWorkspaceInspect = cli.Command{
 	Usage:  "Inspect and report upon the situation of the current workspace (how many modules are there, have we got a cached evaluation of them, etc).",
 	Action: cmdFnWorkspaceInspect,
 	// Aliases: []string{"winspect"}, // doesn't put them at the top level.  Womp.
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:  "gohard",
+			Usage: "whether to spend effort checking the health of modules found; if false, just list them.",
+			Value: true,
+		},
+	},
 }
 
 func cmdFnWorkspaceInspect(c *cli.Context) error {
@@ -33,7 +41,7 @@ func cmdFnWorkspaceInspect(c *cli.Context) error {
 	fmt.Fprintf(c.App.Writer, "Workspace: %s%s\n", wsFs, wsPath)
 
 	// Search for modules within the workspace.
-	fs.WalkDir(wsFs, wsPath, func(path string, d fs.DirEntry, err error) error {
+	return fs.WalkDir(wsFs, wsPath, func(path string, d fs.DirEntry, err error) error {
 		// fmt.Fprintf(c.App.Writer, "hi: %s%s\n", wsFs, path)
 
 		if err != nil {
@@ -66,12 +74,50 @@ func cmdFnWorkspaceInspect(c *cli.Context) error {
 				modName = "!!Unknown!!"
 			}
 
+			everythingParses := false
+			importsResolve := false
+			noticeIngestUsage := false
+			noticeMountUsage := false
+			havePacksCached := false // maybe should have a variant for "or we have a replay we're hopeful about"?
+			haveRunrecord := false
+			haveHappyExit := false
+			if c.Bool("gohard") {
+				if err != nil {
+					goto _checksDone
+				}
+				plot, err := dab.PlotFromFile(wsFs, filepath.Join(filepath.Dir(path), dab.MagicFilename_Plot))
+				if err != nil {
+					goto _checksDone
+				}
+				everythingParses = true
+				plotStats, err := plotexec.ComputeStats(plot, wss)
+				if err != nil {
+					return err // if it's hardcore catalog errors, rather than just unresolvables, I'm out
+				}
+				if plotStats.ResolvableCatalogInputs == plotStats.InputsUsingCatalog {
+					importsResolve = true
+				}
+				if plotStats.InputsUsingIngest > 0 {
+					noticeIngestUsage = true
+				}
+				if plotStats.InputsUsingMount > 0 {
+					noticeMountUsage = true
+				}
+				// TODO: havePacksCached is not supported right now :(
+				// TODO: haveRunrecord needs to both do resolve, and go peek at memos, and yet (obviously) not actually run.
+				// TODO: haveHappyExit needs the above.
+			}
+		_checksDone:
+
 			// Tell me about it.
-			fmt.Fprintf(c.App.Writer, "Module found: %q -- at path %q\n", modName, modPathWithinWs)
+			fmt.Fprintf(c.App.Writer, "Module found: %q -- at path %q", modName, modPathWithinWs)
+			if c.Bool("gohard") {
+				fmt.Fprintf(c.App.Writer, " -- %v %v %v %v %v %v %v",
+					everythingParses, importsResolve, noticeIngestUsage, noticeMountUsage, havePacksCached, haveRunrecord, haveHappyExit)
+			}
+			fmt.Fprintf(c.App.Writer, "\n")
 		}
 
 		return nil
 	})
-
-	return nil
 }
