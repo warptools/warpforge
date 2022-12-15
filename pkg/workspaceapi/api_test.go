1ed70cc2c65dd221d58f4ab7364533356b6dd3de
Eric Myhre <hash@exultant.us>
2022-12-15 12:27:33 +0100

    Sketch of possible API for local workspace situation reporting.
    This commit is foremost an outline and proof-of-concept that we can
    split definitions of our schema into multiple files, and reuse the
    main warpforge API schema types in this second, superset schema.
    
    The actual message types declared in this diff are a sketch only,
    and not firmly committed to.

---
+++ pkg/workspaceapi/api_test.go
@@ -0,0 +1,4 @@
+package workspaceapi
+
+// file exists just to make sure some test files exist, and thus package init is exercised, and thus we test it doesn't panic.
+// drop the above comment when we get more actual test content.
